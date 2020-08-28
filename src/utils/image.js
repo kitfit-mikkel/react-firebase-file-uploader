eb9b1c41987188bafe1e29b8d30618e53ecdea4b
Mikkel <mikkel@kit.fit>
2020-08-28 10:59:55 +1000

    Remove redundant code

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,96 +1,17 @@
 // @flow
 import addToBlobPolyfill from "./polyfill";
 
-// Modified from https://stackoverflow.com/a/32490603, cc by-sa 3.0
-// -2 = not jpeg, -1 = no data, 1..8 = orientations
-function getExifOrientation(file: File): Promise<number> {
-  return new Promise((resolve, reject) => {
-    // Suggestion from http://code.flickr.net/2012/06/01/parsing-exif-client-side-using-javascript-2/:
-    if (file.slice) {
-      file = file.slice(0, 131072);
-    } else if (file.webkitSlice) {
-      file = file.webkitSlice(0, 131072);
-    }
 
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const view = new DataView(e.target.result);
-      if (view.getUint16(0, false) != 0xffd8) {
-        return resolve(-2);
-      }
-      const length = view.byteLength;
-      let offset = 2;
-      while (offset < length) {
-        let marker = view.getUint16(offset, false);
-        offset += 2;
-        if (marker == 0xffe1) {
-          if (view.getUint32((offset += 2), false) != 0x45786966) {
-            return resolve(-1);
-          }
-          const little = view.getUint16((offset += 6), false) == 0x4949;
-          offset += view.getUint32(offset + 4, little);
-          const tags = view.getUint16(offset, little);
-          offset += 2;
-          for (let i = 0; i < tags; i++)
-            if (view.getUint16(offset + i * 12, little) == 0x0112) {
-              return resolve(view.getUint16(offset + i * 12 + 8, little));
-            }
-        } else if ((marker & 0xff00) != 0xff00) break;
-        else offset += view.getUint16(offset, false);
-      }
-      return resolve(-1);
-    };
-    reader.readAsArrayBuffer(file);
-  });
-}
 
-// Derived from https://stackoverflow.com/a/40867559, cc by-sa
-function imgToCanvasWithOrientation(
+function imgToCanvas(
   image: Image,
   rawWidth: number,
-  rawHeight: number,
-  orientation: number
+  rawHeight: number
 ) {
   const canvas = document.createElement("canvas");
 	canvas.width = rawWidth;
 	canvas.height = rawHeight;
-  /*
-  if (orientation > 4) {
-    canvas.width = rawHeight;
-    canvas.height = rawWidth;
-  } else {
-    canvas.width = rawWidth;
-    canvas.height = rawHeight;
-  }
-  */
-
   const ctx = canvas.getContext("2d");
-  //	As browsers now support orientation, and we cannot remove the orientation header, we shall not rotate the image.
-  /*
-  switch (orientation) {
-    case 2:
-      ctx.transform(-1, 0, 0, 1, rawWidth, 0);
-      break;
-    case 3:
-      ctx.transform(-1, 0, 0, -1, rawWidth, rawHeight);
-      break;
-    case 4:
-      ctx.transform(1, 0, 0, -1, 0, rawHeight);
-      break;
-    case 5:
-      ctx.transform(0, 1, 1, 0, 0, 0);
-      break;
-    case 6:
-      ctx.transform(0, 1, -1, 0, rawHeight, 0);
-      break;
-    case 7:
-      ctx.transform(0, -1, -1, 0, rawHeight, rawWidth);
-      break;
-    case 8:
-      ctx.transform(0, -1, 1, 0, 0, rawWidth);
-      break;
-  }
-  */
   ctx.drawImage(image, 0, 0, rawWidth, rawHeight);
   return canvas;
 }
@@ -129,11 +50,10 @@ export default function resizeAndCropImage(
             width = Math.min(maxWidth, image.width);
             height = Math.min(maxHeight, image.height);
           }
-          const canvas = imgToCanvasWithOrientation(
+          const canvas = imgToCanvas(
             image,
             width,
-            height,
-            orientation
+            height
           );
           canvas.toBlob(resolve, file.type, quality);
         });
